4e87efd1c30ab60ba5fad82ba16442b33d9e1ec2
Noah Mercer <noah.mercer@gmail.com>
2024-10-31 14:23:34 -0700

    Changes personalized pace calculation to match spec

--- client/test/utils.test.js
+++ client/test/utils.test.js
@@ -1,55 +1,34 @@
-import { calculatePersonalizedPace, slowBreathsPerMinute, slowerBreathsPerMinute, hzToBreathsPerMinute } from "../src/utils";
+import { calculatePersonalizedPace, defaultBreathsPerMinute, hzToBreathsPerMinute } from "../src/utils";
 
 describe("calculatePersonalizedPaces", () => {
-    it("should throw an error if the slowOrSlower parameter is not 'slow' or 'slower'", () => {
-        expect(() => {
-            calculatePersonalizedPace('fastest!',  [
-                {slowX: 0.012, slowY: 1.0, slowerX: 2.012, slowerY: 3.0}, 
-                {slowX: 0.112, slowY: 1.1, slowerX: 2.112, slowerY: 3.1},
-                {slowX: 0.212, slowY: 1.2, slowerX: 2.212, slowerY: 3.2},
-                {slowX: 0.312, slowY: 1.3, slowerX: 2.312, slowerY: 3.3},
-            ]);
-        }).toThrow()
-    });
-
     it("should throw an error if the peakFreqs parameter has a length other than four", () => {
         expect(() => {
-            calculatePersonalizedPace('slow', [{slowX: 0.0, slowY: 1.0, slowerX: 2.0, slowerY: 3.0}]);
+            calculatePersonalizedPace([{slowX: 0.0, slowY: 1.0, slowerX: 2.0, slowerY: 3.0}]);
         }).toThrow();
     });
 
-    it("should return the default slow pace if all of the slow peak frequencies have x values of n/a and the slowOrSlower parameter is 'slow'", () => {
-        const pace = calculatePersonalizedPace('slow',  [
+    it("should return the default pace if all of the slow peak frequencies have x values of n/a", () => {
+        const pace = calculatePersonalizedPace([
             {slowX: 'n/a', slowY: 1.0, slowerX: 2.0, slowerY: 3.0}, 
             {slowX: 'n/a', slowY: 1.1, slowerX: 2.1, slowerY: 3.1},
             {slowX: 'n/a', slowY: 1.2, slowerX: 2.2, slowerY: 3.2},
             {slowX: 'n/a', slowY: 1.3, slowerX: 2.3, slowerY: 3.3},
         ]);
-        expect(pace).toBe(slowBreathsPerMinute);
-    });
-
-    it("should return the default slower pace if all of the slower peak frequencies have x values of n/a and the slowOrSlower parameter is 'slower'", () => {
-        const pace = calculatePersonalizedPace('slower',  [
-            {slowX: 0.0, slowY: 1.0, slowerX: 'n/a', slowerY: 3.0}, 
-            {slowX: 0.1, slowY: 1.1, slowerX: 'n/a', slowerY: 3.1},
-            {slowX: 0.2, slowY: 1.2, slowerX: 'n/a', slowerY: 3.2},
-            {slowX: 0.3, slowY: 1.3, slowerX: 'n/a', slowerY: 3.3},
-        ]);
-        expect(pace).toBe(slowerBreathsPerMinute);
+        expect(pace).toBe(defaultBreathsPerMinute);
     });
 
     it("should return the only available frequency if three values are n/a", () => {
         const testPeaks = [
-            {slowX: 0.0, slowY: 1.0, slowerX: 'n/a', slowerY: 3.0}, 
-            {slowX: 0.1, slowY: 1.1, slowerX: 'n/a', slowerY: 3.1},
-            {slowX: 0.2, slowY: 1.2, slowerX: 'n/a', slowerY: 3.2},
+            {slowX: 'n/a', slowY: 1.0, slowerX: 0.0, slowerY: 3.0}, 
+            {slowX: 'n/a', slowY: 1.1, slowerX: 0.1, slowerY: 3.1},
+            {slowX: 'n/a', slowY: 1.2, slowerX: 0.2, slowerY: 3.2},
             {slowX: 0.3, slowY: 1.3, slowerX: 0.068, slowerY: 3.3},
         ];
-        const validPaces = testPeaks.filter(p => p.slowerX !== 'n/a');
+        const validPaces = testPeaks.filter(p => p.slowX !== 'n/a');
         expect (validPaces.length).toBe(1);
         const validPace = validPaces[0];
-        const pace = calculatePersonalizedPace('slower',  testPeaks);
-        expect(pace).toBe(hzToBreathsPerMinute(validPace.slowerX));
+        const pace = calculatePersonalizedPace(testPeaks);
+        expect(pace).toBe(hzToBreathsPerMinute(validPace.slowX));
     });
 
     it("should return the frequency that appears most often", () => {
@@ -73,7 +52,7 @@ describe("calculatePersonalizedPaces", () => {
             }
         });
         expect(modalFreq).not.toBe(null);
-        const pace = calculatePersonalizedPace('slow', testPeaks);
+        const pace = calculatePersonalizedPace(testPeaks);
         expect(pace).toBe(hzToBreathsPerMinute(modalFreq));
     });
 
@@ -87,25 +66,25 @@ describe("calculatePersonalizedPaces", () => {
         let maxY = -Infinity;
         let targetX = null;
         testPeaks.forEach(p => {
-            if (p.slowerY > maxY) {
-                maxY = p.slowerY;
-                targetX = p.slowerX;
+            if (p.slowY > maxY) {
+                maxY = p.slowY;
+                targetX = p.slowX;
             }
         });
         expect(targetX).not.toBe(null);
-        const pace = calculatePersonalizedPace('slower', testPeaks);
+        const pace = calculatePersonalizedPace(testPeaks);
         expect(pace).toBe(hzToBreathsPerMinute(targetX));
     });
 
     it("should throw an error if there are no valid Y values and no frequency appears more than once", () => {
         const testPeaks = [
-            {slowX: 0.012, slowY: 1.0, slowerX: 2.012, slowerY: 'n/a'}, 
-            {slowX: 0.112, slowY: 1.1, slowerX: 'n/a', slowerY: 'n/a'},
-            {slowX: 0.212, slowY: 1.2, slowerX: 2.212, slowerY: 'n/a'},
-            {slowX: 0.312, slowY: 1.3, slowerX: 2.312, slowerY: 'n/a'},
+            {slowX: 0.012, slowY: 'n/a', slowerX: 2.012, slowerY: 1.0}, 
+            {slowX: 0.112, slowY: 'n/a', slowerX: 'n/a', slowerY: 1.1},
+            {slowX: 0.212, slowY: 'n/a', slowerX: 2.212, slowerY: 1.2},
+            {slowX: 0.312, slowY: 'n/a', slowerX: 2.312, slowerY: 1.3},
         ];
         expect(() => {
-            calculatePersonalizedPace('slower', testPeaks)
+            calculatePersonalizedPace(testPeaks)
         }).toThrow();
     });
 
