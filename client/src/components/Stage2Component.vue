80d5f909d39c147aa0d9d3a59bed39478175e428
Noah Mercer <noah.mercer@gmail.com>
2024-11-07 14:25:34 -0800

    Conforms all done text to spec

--- client/src/components/Stage2Component.vue
+++ client/src/components/Stage2Component.vue
@@ -2,12 +2,17 @@
     <div>
         <div id="waiting" v-show="!waitOver">
             {{  waitMessage }} 
-            <TimerComponent @timerFinished="waitOver=true" :secondsDuration=600 :showButtons=false :countBy="'seconds'" ref="timer" />
+            <TimerComponent @timerFinished="waitOver=true" :secondsDuration=6 :showButtons=false :countBy="'seconds'" ref="timer" />
         </div>
         <div id="breathing" v-show="waitOver">
-            <RestComponent :key="heartMeasurementCount" :secondsDuration="120" @timerFinished="resetWait">
+            <RestComponent :key="heartMeasurementCount" :secondsDuration="60" @timerFinished="resetWait">
                 <template #preText>
                     Now you will be asked to sit quietly for two minutes with a pulse sensor on your ear to measure your heart rate.
+                </template>
+                <template #postText>
+                    You're all done for today! For this part of the study, you will repeat what you did today: play brain games and have your heart rate measured twice daily. When we're done with these heart rate measurements, you will begin the next part of the study: brain training plus daily mindfulness practice focusing on the breath.
+                    <br/>
+                    <button class="button"  @click="quit">Quit</button>
                 </template>
             </RestComponent>
         </div>
@@ -17,6 +22,7 @@
     import { ref, onMounted } from '@vue/runtime-core'
     import TimerComponent from './TimerComponent.vue'
     import RestComponent from './RestComponent.vue';
+    import { quit } from '../utils'
 
     const timer = ref(null)
     const waitOver = ref(false)
