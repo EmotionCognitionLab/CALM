96de83bec43cb102271cdab3be59e26ba9f001ae
Noah Mercer <noah.mercer@gmail.com>
2024-12-03 14:37:25 -0800

    Latest versions of electron-forge and better-sqlite3

--- client/vite.renderer.config.mjs
+++ client/vite.renderer.config.mjs
@@ -1,37 +1,11 @@
 import { defineConfig } from 'vite';
-import { pluginExposeRenderer } from './vite.base.config.mjs';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config
-export default defineConfig((env) => {
-       const forgeEnv = env;
-       const { root, mode, forgeConfigSelf } = forgeEnv;
-       const name = forgeConfigSelf.name ?? '';
+export default defineConfig(
+    {
+        plugins: [vue()],
+        build: { target: 'esnext' }
 
-       return {
-              root,
-              mode,
-              base: './',
-              build: {
-                     outDir: `.vite/renderer/${name}`
-              },
-              plugins: [pluginExposeRenderer(name), vue()],
-              define: {
-                     'process.env.NODE_DEBUG': false
-              },
-              optimizeDeps: {
-                     esbuildOptions: {
-                            // Node.js global to browser globalThis
-                            // amazon-cognito-auth-js expects
-                            // global.btoa and global.atob to exist
-                            define: {
-                                   global: 'globalThis',
-                            },
-                     },
-              },
-              resolve: {
-                     preserveSymLinks: true,
-              },
-              clearScreen: false
-       }   
-});
+    }
+);
