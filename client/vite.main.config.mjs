4d54601c18b88c2d021dad20f40cd51aaa7b7bc9
Noah Mercer <noah.mercer@gmail.com>
2024-12-05 16:30:40 -0800

    Externalizes node builtins

--- client/vite.main.config.mjs
+++ client/vite.main.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import { builtinModules } from "node:module";
 
 // https://vitejs.dev/config
 export default defineConfig({
     build: {
         rollupOptions: {
-            external: ["electron", "better-sqlite3"]
+            external: [...builtinModules, "electron", "better-sqlite3"]
         }
     }
 });
