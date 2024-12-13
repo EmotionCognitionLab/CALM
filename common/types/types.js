f487211db7b4740589927b873f86c94222fdd339
Noah Mercer <noah.mercer@gmail.com>
2024-12-13 11:29:45 -0800

    Aligns earnings types to spec

--- common/types/types.js
+++ common/types/types.js
@@ -1,21 +1,15 @@
 export const earningsTypes = Object.freeze({
-    STREAK_BONUS: 'streak_bonus',
-    BREATH1: 'breath1',
-    COMPLETION_BREATH2: 'c_breath2',
-    PERFORMANCE_BREATH2: 'p_breath2',
-    TOP_66: 'top_66',
-    TOP_25: 'top_25',
+    PER_HOUR: 'per_hour',
+    LUMOSITY: 'lumosity',
+    BONUS: 'bonus',
     VISIT_1: 'visit1',
     VISIT_2: 'visit2'
 });
 
 export const earningsAmounts = Object.freeze({
-    [earningsTypes.STREAK_BONUS]: 5,
-    [earningsTypes.BREATH1]: 4,
-    [earningsTypes.COMPLETION_BREATH2]: 8,
-    [earningsTypes.PERFORMANCE_BREATH2]: 4,
-    [earningsTypes.TOP_66]: 3,
-    [earningsTypes.TOP_25]: 4,
+    [earningsTypes.PER_HOUR]: 10,
+    [earningsTypes.LUMOSITY]: 2,
+    [earningsTypes.BONUS]: 6,
     [earningsTypes.VISIT_1]: 25,
     [earningsTypes.VISIT_2]: 25
 
