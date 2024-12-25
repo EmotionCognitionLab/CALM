cd7ae5bb5347b60771308ef339a403d32151b722
Noah Mercer <noah.mercer@gmail.com>
2024-12-24 21:30:25 -0800

    Disables login button on click

--- client/src/components/LoginComponent.vue
+++ client/src/components/LoginComponent.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <slot name="bodyText">Please log in to continue.<br/></slot>
-        <button id="startSetup" @click="login">
+        <button :disabled="hasClicked" id="startSetup" @click="login">
             <slot name="btnText">Login</slot>
         </button>
     </div>
 </template>
 <script setup>
+    import { ref } from "vue";
     import { useRoute } from "vue-router";
 
     const route = useRoute();
+    const hasClicked = ref(false);
 
     const login = () => {
+        hasClicked.value = true;
         if (route.query.postLoginPath !== undefined) {
             window.sessionStorage.setItem('postLoginPath', route.query.postLoginPath)
         }
