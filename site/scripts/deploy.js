caf055310918cd0d06ed4827eb595345615de716
Noah Mercer <noah.mercer@gmail.com>
2026-01-21 16:03:13 -0800

    Explicit region for s3 client

--- site/scripts/deploy.js
+++ site/scripts/deploy.js
@@ -70,7 +70,7 @@ async function invalidateCloudFrontDistribution(distId, CloudFrontClient, Create
 
 (async () => {
     const { S3Client, Upload, CloudFrontClient, CreateInvalidationCommand } = await initClients();
-    const s3Client = new S3Client();
+    const s3Client = new S3Client({region: 'us-east-2' });
     await readDir(distDir, s3Client, Upload);
     await invalidateCloudFrontDistribution(awsSettings.CloudFrontDistributionId, CloudFrontClient, CreateInvalidationCommand);
 })().catch(err => console.error('Error:', err));
